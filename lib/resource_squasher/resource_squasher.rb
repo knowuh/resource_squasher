cba57d9d33bace23afaf912af08646d300c72dc2
Richard Klancer <rpk@pobox.com>
2011-04-28 17:40:03 -0400

    Replace static/my_system/en/current with static/my_system/en/<build number> throughout

--- lib/resource_squasher/resource_squasher.rb
+++ lib/resource_squasher/resource_squasher.rb
@@ -6,6 +6,8 @@ module ResourceSquasher
     attr_accessor :rez_base
     attr_accessor :file_mapper
     attr_accessor :output_dir
+    attr_accessor :language
+    attr_accessor :build_number
 
     def self.initialize
       @project_name = nil
@@ -38,10 +40,12 @@ module ResourceSquasher
         :rez_base     => DEFAULT_REZ_BASE
       }
       opts                          = defaults.merge(_opts)
-      ResourceSquasher.project_name= opts[:project_name]
+      ResourceSquasher.project_name = opts[:project_name]
       self.output_dir               = opts[:output_dir]
       self.source_dir               = opts[:source_dir]
       self.rez_base                 = opts[:rez_base]
+      self.language                 = opts[:language]     || DEFAULT_LANGUAGE
+      self.build_number             = `sc-build-number #{ResourceSquasher.project_name}`
       self.file_mapper              = FileMapper.new(opts)
     end
 
@@ -52,7 +56,7 @@ module ResourceSquasher
 
     #  tmp/build/static/my_system/en/67bd8352e47bfe3a4cabf92df08ef2022c7368a7/
     def most_recent_project_html
-      matching = /[a-z|0-9]{40}/ #TODO: This signature is only valid against sproutcore builds.
+      matching = /#{self.build_number}/ #TODO: This signature is only valid against sproutcore builds.
       parent_dir = Dir.new(project_dir)
       builds = parent_dir.entries.collect { |file| File.new(File.join(parent_dir.path,file)) }.sort { |file1,file2| file1.mtime <=> file2.mtime }
       builds.reject! { |file| ((file.path =~ matching) ? false : true) }
@@ -77,6 +81,17 @@ module ResourceSquasher
     def load_resources(filename)
       file = File.new(filename)
       content = file.read
+      file.close
+         
+      # replace 'static/my_system/en/current/' => 'static/my_system/en/fe766a8e4c82f8b387effb309563f4fd114248ee/' 
+      # (where fe766... is current build number)
+      
+      content.gsub!(/#{self.rez_base}\/#{ResourceSquasher.project_name}\/#{self.language}\/current\//, "#{self.rez_base}/#{ResourceSquasher.project_name}/#{self.language}/#{self.build_number}/")
+
+      out = File.new(filename, 'w')
+      out.write(content)
+      out.close
+      
       content.scan(resource_regex) do  |mgroup|
         resource = mgroup.first
         #TODO: return value for add_file is true if new file
